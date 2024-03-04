d840b96c3bf032aa9a450fbe291fce4b0739f730
JEROLPOA2 <jledesmap@unal.edu.co>
2024-03-04 11:39:07 -0500

    update

--- pregunta.py
+++ pregunta.py
@@ -7,14 +7,40 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
 import pandas as pd
-
+import re
+from datetime import datetime
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
+
+    for i in df.columns:
+        
+        try:
+            df[i] = df[i].str.lower().str.strip()
+        except:
+            pass
+
+    df["barrio"] = df["barrio"].str.replace(r'[^a-zA-Z0-9]', '')
+    
+    df["fecha_de_beneficio"] = pd.to_datetime(df['fecha_de_beneficio'], 
+                                              format='%d/%m/%Y', 
+                                              errors='coerce')
+    
+    df["fecha_de_beneficio"] = df["fecha_de_beneficio"].fillna(pd.to_datetime(df['fecha_de_beneficio'], 
+                                                                              format='%Y/%m/%d', errors='coerce'))
+
+    df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int)
+    
+    df["monto_del_credito"] = df["monto_del_credito"].str.replace(r'[^a-zA-Z0-9]', '')
+    df["monto_del_credito"] = df["monto_del_credito"].astype(float)
+
+    print(df)
 
     #
     # Inserte su código aquí
     #
 
     return df
+
+clean_data()
